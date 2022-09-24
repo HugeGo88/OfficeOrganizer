ebdfc4db412113f9591ac7af35b9edbb7005f16b
Hugo Tausch <hugo.tausch@gmail.com>
2022-09-24 13:18:03 +0200

    Added Letter Model

---
+++ OfficeOrganizer.Core/Models/Letter.cs
@@ -0,0 +1,53 @@
+﻿namespace OfficeOrganizer.Core.Models;
+public class Letter
+{
+    public bool Header
+    {
+        get; set;
+    }
+
+    public string Company
+    {
+        get; set;
+    }
+
+    public string FirstName
+    {
+        get; set;
+    }
+
+    public string LastName
+    {
+        get; set;
+    }
+
+    public string Street
+    {
+        get; set;
+    }
+
+    public string City
+    {
+        get; set;
+    }
+
+    public bool Signing
+    {
+        get; set;
+    }
+
+    public DateTimeOffset SetDate
+    {
+        get; set;
+    }
+
+    public bool CustomeDate
+    {
+        get; set;
+    }
+
+    public string Content
+    {
+        get; set;
+    }
+}
