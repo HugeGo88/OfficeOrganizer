ef4802200b12d370e3c57762e12d2d9c0cea6a7e
Hugo Tausch <hugo.tausch@gmail.com>
2022-09-24 16:12:15 +0200

    Added properties to writerviewmodel

--- OfficeOrganizer/ViewModels/WriterViewModel.cs
+++ OfficeOrganizer/ViewModels/WriterViewModel.cs
@@ -1,10 +1,59 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace OfficeOrganizer.ViewModels;
-public class WriterViewModel : ObservableRecipient
-{
 
+public partial class WriterViewModel : ObservableObject
+{
     public WriterViewModel()
     {
     }
+    [ObservableProperty]
+    string? firstName;
+
+    [ObservableProperty]
+    string? lastName;
+
+    [ObservableProperty]
+    string? city;
+
+    [ObservableProperty]
+    string? street;
+
+    [ObservableProperty]
+    string? company;
+
+    [ObservableProperty]
+    string? content;
+
+    [ObservableProperty]
+    DateTimeOffset setDate;
+
+    [ObservableProperty]
+    bool header;
+
+    [ObservableProperty]
+    bool customeDate;
+
+    [ObservableProperty]
+    bool signing;
+
+    [ICommand]
+    void Save()
+    {
+        Console.WriteLine($"{LastName} {FirstName}");
+    }
+
+    [ICommand]
+    void Load()
+    {
+        Console.WriteLine($"{LastName} {FirstName}");
+    }
+
+    [ICommand]
+    void Create()
+    {
+        Console.WriteLine($"{LastName} {FirstName}");
+    }
+
 }
